537bcba6344e301a1c7f81ceb75293de285aee78
Alexandre Rodrigues <alexandre.ro1222@gmail.com>
2021-08-23 03:06:50 -0300

    Segunda versao da cifra de ceasar semi-funcional

--- ceasar/src/main/java/com/ceasar/Cipher.java
+++ ceasar/src/main/java/com/ceasar/Cipher.java
@@ -1,30 +1,34 @@
 package com.ceasar;
 
+import java.lang.reflect.Array;
+
 public class Cipher {
 
     public static void main(String[] args) {
         String texto = "Teste";
-        String mesclado = "";
-        for(int i = 0; i < texto.length(); i++){
-            int convert = ((int)texto.charAt(i)) + 3;
-            if(convert > 122){
-                convert -= 25;
-            }
-            if(convert > 90 && convert < 96){
-                convert -= 25;
-            }
-            
-            char desconv = (char) convert;
-            mesclado = String.valueOf(desconv);
-            mesclado.concat(mesclado);
-            
-           
-        }
-        System.out.print(mesclado);
+        char[] str = new char[texto.length()];
+        String concatenar = new String();
+           // int convert = ((int)texto.charAt(i)) + 3;
+           for(int i = 0; i < texto.length(); i++){
+
+               int convert = ((int)texto.charAt(i)) + 3;
+                str[i] = (char)convert;
+                 concatenar = new String(str);
+               System.out.println(concatenar);
+           }
     }
 
     public String descriptar(String n){
-        return "Teste";
+        char[] str = new char[n.length()];
+        String concatenar = new String();
+           // int convert = ((int)texto.charAt(i)) + 3;
+           for(int i = 0; i < n.length(); i++){
+
+               int convert = ((int)n.charAt(i)) + 3;
+                str[i] = (char)convert;
+                 concatenar = new String(str);
+           }
+        return concatenar;
         
     }
 }
